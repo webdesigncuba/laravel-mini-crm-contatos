bef81b8567d3bfa479a0d8e4f85aea636a8f0649
David Cordero <webdesigncuba@gmail.com>
2026-05-14 23:40:13 -0300

    feat(request): Refatoração do ProcessContactScoreRequest

--- app/Infrastructure/Http/Requests/ProcessContactScoreRequest.php
+++ app/Infrastructure/Http/Requests/ProcessContactScoreRequest.php
@@ -8,21 +8,20 @@ class ProcessContactScoreRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        // Aqui você pode aplicar regras de autorização
+
         return true;
     }
 
     public function rules(): array
     {
         return [
-            'id' => ['required', 'integer', 'exists:contacts,id'],
+            'id' => ['integer', 'exists:contacts,id'],
         ];
     }
 
     public function messages(): array
     {
         return [
-            'id.required' => 'O ID do contato é obrigatório.',
             'id.integer'  => 'O ID deve ser um número inteiro.',
             'id.exists'   => 'Contato não encontrado.',
         ];
